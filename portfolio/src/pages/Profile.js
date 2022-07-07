038a700d7e21c4029abe232ebeeea73463ed5246
healingfields <dihratene@gmail.com>
2022-07-07 20:42:07 +0100

    created Profile page

---
+++ portfolio/src/pages/Profile.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import './Profile.css';
+import Link from '../components/Link';
+import List from '../components/List';
+
+function Profile({ username }) {
+    const [loading, setLoading] = useState(false);
+    const [profile, setProfile] = useState({});
+    const items = [
+        {
+            field: 'html_url',
+            value: <Link url={profile.html_url}
+                title={profile.html_url} />,
+        },
+        {
+            field: 'repos_url',
+            value: <Link url={profile.repos_url}
+                title={profile.repos_url} />
+        },
+        { field: 'name', value: profile.name },
+        { field: 'location', value: profile.location },
+        { field: 'bio', value: profile.bio }
+    ];
+
+    useEffect(() => {
+        async function fetchData() {
+            const profile = await fetch(
+                `https://api.github.com/users/${username}`
+            );
+            const result = await profile.json();
+
+            if (result) {
+                setLoading(false);
+                setProfile(result);
+            }
+        }
+
+        fetchData();
+    }, [username])
+
+    return (
+        <div className='profile-container'>
+            <h2>About me</h2>
+            {loading ?
+                (<spa>Loading...</spa>)
+                :
+                (
+                    <div>
+                        <img src={profile.avatar_url} alt={profile.name} className='profile-avatar' />
+                        <List items={items} />
+                    </div>
+                )
+            }
+
+        </div>
+    )
+
+}
+
+export default Profile
