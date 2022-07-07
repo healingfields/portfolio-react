e7b5c2b909aaeef13b0533c00067edb0e90823f9
healingfields <dihratene@gmail.com>
2022-07-07 20:42:48 +0100

    installed react router

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './assets/logo.svg';
+import './App.css';
+
+import Header from './components/Header';
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Header logo={logo} />
+      <Profile username="healingfields" />
+    </div>
+  );
+}
+
+export default App;
